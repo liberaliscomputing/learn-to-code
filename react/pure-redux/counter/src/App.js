00a59bd73817f4f93781e39c8530cd3c05367c05
liberaliscomputing <liberalis.computing@gmail.com>
2019-03-12 09:36:56 -0400

    :sparkles: start react/pure-redux

---
+++ react/pure-redux/counter/src/App.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import Counter from './components/Counter';
+
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ react/pure-redux/counter/src/actions/index.js
@@ -0,0 +1,5 @@
+import { DECREASE, INCREASE } from '../constants/action-types';
+
+
+export const decrease = () => ({ type: DECREASE });
+export const increase = () => ({ type: INCREASE });
